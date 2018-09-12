9caf1d8beae1b778ca7b8c3796c4b72bbc3bc405
Micah Lyle <micahlyle@gmail.com>
2018-09-12 10:03:29 -0400

    Fixed urls

--- django_slack_oauth/urls.py
+++ django_slack_oauth/urls.py
@@ -8,6 +8,6 @@ from .views import SlackAuthView, DefaultAddSuccessView, DefaultSigninSuccessVie
 urlpatterns = [
     url('add/', SlackAuthView.as_view(auth_type="add"), name='slack_add'),
     url('signin/', SlackAuthView.as_view(auth_type="signin"), name='slack_signin'),
-    url('add-success/', DefaultAddSuccessView.as_view(), name='slack_add_success')
+    url('add-success/', DefaultAddSuccessView.as_view(), name='slack_add_success'),
     url('signin-success/', DefaultSigninSuccessView.as_view(), name='slack_signin_success')
 ]
